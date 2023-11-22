49fed9787fa0bfd765865d289863447cb976bb06
김성현 <oilater@naver.com>
2023-11-22 09:35:10 +0900

    signup

--- src/pages/Landing/components/login/modal.js
+++ src/pages/Landing/components/login/modal.js
@@ -19,12 +19,28 @@ const Modal = ({ closeModal }) => {
   // 로그인 > 유저 입력 (아이디, 비밀번호)
   const [userId, setUserId] = useState("");
   const [userPassword, setUserPassword] = useState("");
+
+  // 회원가입 > 유저 입력 (이름, 아이디, 비밀번호, 이메일)
+  const [signupName, setSignupName] = useState("");
+  const [signupId, setSignupId] = useState("");
+  const [signupPassword, setSignupPassword] = useState("");
+  const [signupEmail, setSignupEmail] = useState("");
+
+  // 로그인, 회원가입 클릭 여부
   const [isLogin, setIsLogin] = useRecoilState(loginState);
+
+  //로그인 토큰
   const [loginToken, setLoginToken] = useRecoilState(loginTokenState);
   const setLoginedUser = useSetRecoilState(loginedUserState);
-  const loginedUser = useRecoilValue(loginedUserState); // 로그인한 유저의 정보 (디코딩해서 받아옴)
+
+  // 로그인한 유저의 정보 (토큰을 디코딩해서 받아옴)
+  const loginedUser = useRecoilValue(loginedUserState);
+
+  // 관리자 페이지 이동
   const navigate = useNavigate();
   const { admin } = useLocation();
+
+  // 로그인 value 세팅
   const handleUserId = (e) => {
     setUserId(e.target.value);
   };
@@ -33,6 +49,24 @@ const Modal = ({ closeModal }) => {
     setUserPassword(e.target.value);
   };
 
+  // 회원가입 value 세팅
+  const handleSignupName = (e) => {
+    setSignupName(e.target.value);
+  };
+
+  const handleSignupId = (e) => {
+    setSignupId(e.target.value);
+  };
+
+  const handleSignupPassword = (e) => {
+    setSignupPassword(e.target.value);
+  };
+
+  const handleSignupEmail = (e) => {
+    setSignupEmail(e.target.value);
+  };
+
+  // 로그인 버튼 클릭 시 처리
   const handleLogin = async () => {
     try {
       const loginData = {
@@ -45,19 +79,39 @@ const Modal = ({ closeModal }) => {
         loginData
       );
 
-      console.log(response.data.token); // 토큰 키 받아옴
-      const token = response.data.token;
-      setLoginToken(response.data.token); // 토큰 키 recoil에 세팅
-      setIsLogin(true); // 로그인 상태 true로 전환
+      console.log(response.data.token);
+      const token = response.data.token; // 서버로부터 받아온 토큰 키 저장
+      setLoginToken(response.data.token); // 토큰 키 recoilState에 세팅
+      setIsLogin(true); // 로그인 상태 true
       closeModal(false); // 모달 창 닫기
 
-      // JWT 토큰 디코딩
+      // JWT 토큰 디코딩, 로그인한 유저 정보 저장
       const decodedToken = jwtDecode(token);
       setLoginedUser(decodedToken);
-
       console.log(decodedToken);
     } catch (error) {
-      console.error("로그인 실패, ", error);
+      console.error("로그인 실패! : ", error);
+    }
+  };
+
+  // 회원가입 버튼 클릭시 처리
+  const handleSignup = async () => {
+    try {
+      const signupData = {
+        id: signupId,
+        pw: signupPassword,
+        name: signupName,
+        email: signupEmail,
+      };
+
+      const response = await axios.post(
+        "http://localhost/api/user",
+        signupData
+      );
+      console.log("회원가입 유저 정보", response);
+      setAction("login");
+    } catch (error) {
+      console.log("회원가입 실패! : ", error);
     }
   };
 
@@ -81,52 +135,83 @@ const Modal = ({ closeModal }) => {
         </div>
         <div className="inputs">
           {action === "login" ? (
-            <></>
+            // 로그인 input
+            <>
+              <div className="input">
+                <img src={userIcon} alt="유저 아이디 아이콘" />
+                <input
+                  onChange={handleUserId}
+                  value={userId}
+                  type="text"
+                  placeholder="아이디"
+                />
+              </div>
+              <div className="input">
+                <img src={passwordIcon} alt="비밀번호 아이콘" />
+                <input
+                  onChange={handleUserPassword}
+                  value={userPassword}
+                  type="password"
+                  placeholder="비밀번호"
+                />
+              </div>
+            </>
           ) : (
+            // 회원가입 input
             <>
               <div className="input">
                 <img src={userIcon} alt="유저 이름" />
-                <input type="text" placeholder="이름" />
+                <input
+                  onChange={handleSignupName}
+                  value={signupName}
+                  type="text"
+                  placeholder="이름"
+                />
+              </div>
+              <div className="input">
+                <img src={userIcon} alt="유저 아이디 아이콘" />
+                <input
+                  onChange={handleSignupId}
+                  value={signupId}
+                  type="text"
+                  placeholder="아이디"
+                />
+              </div>
+              <div className="input">
+                <img src={passwordIcon} alt="비밀번호 아이콘" />
+                <input
+                  onChange={handleSignupPassword}
+                  value={signupPassword}
+                  type="password"
+                  placeholder="비밀번호"
+                />
               </div>
               <div className="input">
                 <img src={emailIcon} alt="이메일 아이콘" />
-                <input type="password" placeholder="이메일" />
+                <input
+                  onChange={handleSignupEmail}
+                  value={signupEmail}
+                  type="email"
+                  placeholder="이메일"
+                />
               </div>
             </>
           )}
-
-          <div className="input">
-            <img src={userIcon} alt="유저 아이디 아이콘" />
-            <input
-              onChange={handleUserId}
-              value={userId}
-              type="text"
-              placeholder="아이디"
-            />
-          </div>
-          <div className="input">
-            <img src={passwordIcon} alt="비밀번호 아이콘" />
-            <input
-              onChange={handleUserPassword}
-              value={userPassword}
-              type="password"
-              placeholder="비밀번호"
-            />
-          </div>
         </div>
         <div className="submit-container">
           <div
-            className={action === "login" ? "submit gray" : "submit"}
+            className={action === "login" ? "submit gray" : "submit"} // 버튼 색상 전환
             onClick={() => {
-              setAction("signUp");
+              action === "signup" && handleSignup(); // action이 회원가입일 때만 버튼이 눌림
+              setAction("signup");
             }}
           >
             회원가입
           </div>
           <div
-            className={action === "signUp" ? "submit gray" : "submit"}
+            className={action === "login" ? "submit" : "submit gray"} // 버튼 색상 전환
             onClick={() => {
-              action === "login" && handleLogin(); // action이 로그인일 때만 눌리도록 수정하기
+              action === "login" && handleLogin(); // action이 로그인일 때만 버튼이 눌림
               setAction("login");
             }}
           >
